8f6281ed3b58b562900bb783a3d57e075cbb3a2f
Vikram Voleti <vikky2904@gmail.com>
2019-04-06 03:11:46 -0400

    Removes datasets from gitignore, adds mnist, svhn dataloaders

---
+++ vision/datasets/mnist.py
@@ -0,0 +1,29 @@
+import numpy as np
+import torch
+
+from torch.utils.data import Dataset
+from torchvision import transforms
+from torchvision.datasets import MNIST
+
+
+class MNISTDataset(Dataset):
+    def __init__(self, root_dir, transform=transforms.Compose([
+                                               transforms.ToTensor()])):
+        super().__init__()
+        self.root_dir = root_dir
+        self.transform = transform
+        self.dataset = MNIST(root_dir, transform=transform)
+        self.num_classes = len(self.dataset.classes)
+
+    def __getitem__(self, index):
+        x, y = self.dataset.__getitem__(index)
+        x = torch.cat((x, x, x), dim=0)
+        y_onehot = [0.]*self.num_classes
+        y_onehot[y] = 1.
+        return {
+            "x": x,
+            "y_onehot": np.asarray(y_onehot, dtype=np.float32)
+        }
+
+    def __len__(self):
+        return len(self.dataset)
---
+++ vision/datasets/svhn.py
@@ -0,0 +1,28 @@
+import numpy as np
+import torch
+
+from torch.utils.data import Dataset
+from torchvision import transforms
+from torchvision.datasets import SVHN
+
+
+class SVHNDataset(Dataset):
+    def __init__(self, root_dir, transform=transforms.Compose([
+                                               transforms.ToTensor()])):
+        super().__init__()
+        self.root_dir = root_dir
+        self.transform = transform
+        self.dataset = SVHN(root_dir, transform=transform)
+        self.num_classes = 10
+
+    def __getitem__(self, index):
+        x, y = self.dataset.__getitem__(index)
+        y_onehot = [0.]*self.num_classes
+        y_onehot[y] = 1.
+        return {
+            "x": x,
+            "y_onehot": np.asarray(y_onehot, dtype=np.float32)
+        }
+
+    def __len__(self):
+        return len(self.dataset)
