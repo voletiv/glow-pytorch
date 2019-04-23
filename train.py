d5c8bb947260ca2bf648e91918e7dfb53d9831d6
Vikram Voleti <vikky2904@gmail.com>
2019-04-23 18:18:00 -0400

    Minor changes

--- train.py
+++ train.py
@@ -15,27 +15,33 @@ from glow.trainer_cometml import Trainer
 
 
 if __name__ == "__main__":
+    # Read params
     args = docopt(__doc__)
     hparams = args["<hparams>"]
     dataset_name = args["<dataset_name>"]
     dataset_root = args["<dataset_root>"]
     name = args["<name>"]
+
+    # Checks
     assert dataset_name in vision.Datasets, (
         "`{}` is not supported, use `{}`".format(dataset_name, vision.Datasets.keys()))
     assert os.path.exists(dataset_root), (
         "Failed to find root dir `{}` of dataset.".format(dataset_root))
     assert os.path.exists(hparams), (
-        "Failed to find hparams josn `{}`".format(hparams))
+        "Failed to find hparams json `{}`".format(hparams))
+
+    # Build graph
     hparams = JsonConfig(hparams)
-    dataset_class = vision.Datasets[dataset_name]
-    # set transform of dataset
+    built = build(hparams, True)
+
+    # Build dataset
     transform = transforms.Compose([
         transforms.Resize(hparams.Data.resize),
         transforms.CenterCrop(hparams.Data.center_crop),
         transforms.ToTensor()])
-    # build graph and dataset
-    built = build(hparams, True)
+    dataset_class = vision.Datasets[dataset_name]
     dataset = dataset_class(dataset_root, transform=transform)
-    # begin to train
+
+    # Begin to train
     trainer = Trainer(**built, dataset=dataset, hparams=hparams, name=name, dataset_name=dataset_name)
     trainer.train()
