15c51a52e93b2c7bbb1f682b26b4ca195eb99e8b
Vikram Voleti <vikky2904@gmail.com>
2019-04-29 23:29:48 -0400

    Adds code for taking dataset+type as arg

--- train_adain_cometml.py
+++ train_adain_cometml.py
@@ -1,7 +1,7 @@
 """Train script.
 
 Usage:
-    train_adain_cometml.py <hparams> <dataset1_root> <dataset2_root> <name>
+    train_adain_cometml.py <hparams> <dataset1_type> <dataset1_root> <dataset2_type> <dataset2_root> <name>
 """
 from comet_ml import Experiment
 
@@ -18,7 +18,9 @@ if __name__ == "__main__":
     # Args
     args = docopt(__doc__)
     hparams = args["<hparams>"]
+    dataset1_type = args["<dataset1_type>"]
     dataset1_root = args["<dataset1_root>"]
+    dataset2_type = args["<dataset2_type>"]
     dataset2_root = args["<dataset2_root>"]
     name = args["<name>"]
 
@@ -29,10 +31,14 @@ if __name__ == "__main__":
     # Check
     assert os.path.exists(hparams), (
         "Failed to find hparams json `{}`".format(hparams))
+    assert dataset1_type in vision.Datasets, (
+        "dataset1_type `{}` is not supported, use one of `{}`".format(dataset1_type, vision.Datasets.keys()))
     assert os.path.exists(dataset1_root), (
-        "Failed to find root dir of dataset1: `{}`".format(dataset1_root))
-    assert os.path.exists(dataset1_root), (
-        "Failed to find root dir of dataset2: `{}`".format(dataset2_root))
+        "Failed to find dataset1_root: `{}`".format(dataset1_root))
+    assert dataset2_type in vision.Datasets, (
+        "dataset2_type `{}` is not supported, use one of `{}`".format(dataset2_type, vision.Datasets.keys()))
+    assert os.path.exists(dataset2_root), (
+        "Failed to find dataset2_root: `{}`".format(dataset2_root))
 
     # Build graph
     hparams = JsonConfig(hparams)
@@ -47,8 +53,10 @@ if __name__ == "__main__":
 
     # Build dataset
     # Photoshop problem : https://github.com/python-pillow/Pillow/pull/3771
-    dataset_class = vision.Datasets['adain']
-    dataset = dataset_class(dataset1_root, dataset2_root, transform=transform)
+    adain_class = vision.Datasets['adain']
+    dataset = adain_class(vision.Datasets[dataset1_type], dataset1_root,
+                          vision.Datasets[dataset2_type], dataset2_root,
+                          transform=transform)
 
     # begin to train
     trainer = Trainer(**built, dataset=dataset, hparams=hparams, name=name, dataset_name='adain')
--- vision/datasets/adain.py
+++ vision/datasets/adain.py
@@ -7,22 +7,34 @@ from torchvision.datasets import ImageFolder
 
 
 class AdaINDataset(Dataset):
-    def __init__(self, data1_dir, data2_dir, transform=transforms.Compose([
+    def __init__(self, data1_class, data1_dir, data2_class, data2_dir, transform=transforms.Compose([
                                                 transforms.ToTensor()])):
         super().__init__()
+        self.data1_class = data1_class
         self.data1_dir = data1_dir
+        self.data2_class = data2_class
         self.data2_dir = data2_dir
         self.transform = transform
-        self.dataset1 = ImageFolder(self.data1_dir, transform=transform)
-        self.dataset2 = ImageFolder(self.data2_dir, transform=transform)
+        self.dataset1 = self.data1_class(self.data1_dir, transform=transform)
+        self.dataset2 = self.data2_class(self.data2_dir, transform=transform)
         self.len_dataset1 = len(self.dataset1)
         self.len_dataset2 = len(self.dataset2)
 
     def __getitem__(self, index):
         dataset1_index = index
         dataset2_index = np.random.randint(self.len_dataset2)
-        x1, _ = self.dataset1.__getitem__(dataset1_index)
-        x2, _ = self.dataset2.__getitem__(dataset2_index)
+        data1 = self.dataset1.__getitem__(dataset1_index)
+        data2 = self.dataset2.__getitem__(dataset2_index)
+        # x1
+        if isinstance(self.data1_class, ImageFolder):
+            x1, _ = data1
+        else:
+            x1 = data1["x"]
+        # x2
+        if isinstance(self.data2_class, ImageFolder):
+            x2, _ = data2
+        else:
+            x2 = data2["x"]
         # Add instance noise ~ U(0,1)
         # x1 = (x1*255. + torch.rand(x1.size()))/256.
         # x2 = (x2*255. + torch.rand(x2.size()))/256.
